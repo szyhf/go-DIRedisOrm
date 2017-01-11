dce64a6dbb582ef9ba373a948553dc3b2063d65a
Back Yu <yhfszb@gmail.com>
2017-01-11 16:49:14 +0800

    使用TxPipline代替Pipline

--- redis_pipline.go
+++ redis_pipline.go
@@ -3,7 +3,7 @@ package rorm
 import redis "gopkg.in/redis.v5"
 
 func (r *RedisQuerier) ExecPipeline(fun func(pipe *redis.Pipeline) error) ([]redis.Cmder, error) {
-	pipe := r.Client.Pipeline()
+	pipe := r.Client.TxPipeline()
 	defer pipe.Close()
 	if err := fun(pipe); err == nil {
 		cmderAry, err := pipe.Exec()
